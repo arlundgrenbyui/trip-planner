f5425ef84ccb662b680cd438e55f13b3adf43047
Andrew Lundgren <lun16002@byui.edu>
2021-06-17 14:44:07 -0600

    fixing auth issue

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { body } = require('express-validator/check');
 
 const adminController = require('../controllers/admin');
-const isAuth = require('../middleware/is-auth');
+const isAuth = require('../middlewares/is-auth');
 
 const router = express.Router();
 
